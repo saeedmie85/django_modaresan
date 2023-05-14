daf5dc92d1fec81c4cd637c0fcf06f639f7874ea
saeed <saeedm.ie85@gmail.com>
2023-05-14 18:54:00 +0430

    جلسه 14 بخش اول

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, re_path
-from .views import  category_list, PostList, PostDetail
+from .views import category_list, post_list, PostDetail, author_list
 
 app_name = "blog"
 
 urlpatterns = [
-    # path("", post_list, name="post_list"),
-    path("", PostList.as_view(), name="post_list"),
-    path("post/page/<int:number>", PostList.as_view(), name="post_list"),
-    # path("post/page/<int:number>", post_list, name="post_list"),
+    path("", post_list, name="post_list"),
+    # path("", PostList.as_view(), name="post_list"),
+    # path("page/<int:number>", PostList.as_view(), name="post_list"),
+    path("post/page/<int:number>", post_list, name="post_list"),
     # re_path(r"post/(?P<slug>[-\w]+)", post_detail, name="post_detail"),
     re_path(r"post/(?P<slug>[-\w]+)", PostDetail.as_view(), name="post_detail"),
     path("category/<slug:slug>", category_list, name="category_list"),
     path("category/<slug:slug>/page/<int:number>", category_list, name="category_list"),
+    path("author/<slug:user_name>", author_list, name="author_list"),
+    path("author/<slug:user_name>/page/<int:number>", author_list, name="author_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,23 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Post, Category
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
-# def post_list(request, number=1):
-#     posts_list = Post.objects.filter(status="published")
-#     paginator = Paginator(posts_list, 1)
-#     posts = paginator.get_page(number)
-#     return render(request, "blog\posts\home.html", {"posts": posts})
+def post_list(request, number=1):
+    posts_list = Post.objects.filter(status="published")
+    paginator = Paginator(posts_list, 2)
+    posts = paginator.get_page(number)
+    return render(request, "blog\posts\home.html", {"posts": posts})
 
 
-class PostList(ListView):
-    # model = Post
-    queryset = Post.objects.filter(status="published")
-    context_object_name = "posts"
-    paginate_by = 2
-    template_name = "blog\posts\home.html"
+# class PostList(ListView):
+#     model = Post
+#     # queryset = Post.objects.filter(status="published")
+#     context_object_name = "posts"
+#     paginate_by = 1
+#     template_name = "blog\posts\home.html"
 
 
 # def post_detail(request, slug):
@@ -38,8 +39,18 @@ class PostDetail(DetailView):
 def category_list(request, slug, number=1):
     category = get_object_or_404(Category, slug=slug)
     posts_list = category.post_set.filter(status="published")
-    paginator = Paginator(posts_list, 1)
+    paginator = Paginator(posts_list, 2)
     posts = paginator.get_page(number)
     return render(
         request, "blog\posts\category_list.html", {"posts": posts, "category": category}
+    )
+
+
+def author_list(request, user_name, number=1):
+    author = get_object_or_404(User, username=user_name)
+    posts_list = author.blog_post.filter(status="published")
+    paginator = Paginator(posts_list, 2)
+    posts = paginator.get_page(number)
+    return render(
+        request, "blog/posts/author_list.html", {"posts": posts, "author": author}
     )
