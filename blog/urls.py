767b139e4798728a9aec4aafc6e37362286b8ae5
saeed <saeedm.ie85@gmail.com>
2023-06-18 19:54:44 +0330

    جلسه آخر

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns += [
-    re_path(r"^signup/$", views.signup, name="signup"),
+    re_path(r"^bsignup/$", views.signup, name="signup"),
     re_path(
         r"^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
         views.activate,
